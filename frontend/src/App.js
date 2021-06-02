6db90b553ee445373e0aadc13f3385b9d4cb0b24
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-06-02 16:39:00 +0200

    feat: ex3 & ex-4 integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {Container, Row, Col} from 'react-bootstrap'
 import MyBadge from './components/MyBadge';
 import WarningSign from './components/WarningSign'
 import SingleBook from './components/SingleBook'
+import BookList from "./components/BooksList";
 
 const MockBookObj = {
     "asin": "1250082757",
@@ -12,6 +13,36 @@ const MockBookObj = {
     "price": 26.09,
     "category": "scifi"
 }
+const BooksObjArr = [
+    {
+        "asin": "1940026091",
+        "title": "Pandemic (The Extinction Files, Book 1)",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/91xrEMcvmQL.jpg",
+        "price": 7.81,
+        "category": "scifi"
+    },
+    {
+        "asin": "0425264041",
+        "title": "Shards of Hope (Psy-Changeling Novel, A)",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/91ku%2B0LppPL.jpg",
+        "price": 7.59,
+        "category": "scifi"
+    },
+    {
+        "asin": "1250082757",
+        "title": "Born of Vengeance: The League: Nemesis Rising",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/91J28bj3PYL.jpg",
+        "price": 26.09,
+        "category": "scifi"
+    },
+    {
+        "asin": "045146799X",
+        "title": "Gameboard of the Gods",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/81-vppSolJL.jpg",
+        "price": 7.59,
+        "category": "scifi"
+    }
+]
 
 function App() {
     return (
@@ -23,16 +54,7 @@ function App() {
                     color={"gray"}>
                 </MyBadge>
             </header>
-            <Container>
-                <Row>
-                    <Col/>
-                    <Col>
-                        <SingleBook book={MockBookObj}/>
-                    </Col>
-                    <Col/>
-                </Row>
-            </Container>
-
+            <BookList books={BooksObjArr} > </BookList>
         </div>
     );
 }
--- frontend/src/components/BooksList.jsx
+++ frontend/src/components/BooksList.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import SingleBook from "./SingleBook";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import {Col, Container, Form, Row} from "react-bootstrap";
 
 class BookList extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Row>
-          books.map((x) => (
-          <Col xs={3}>
-            <SingleBook book={x} />
-          </Col>
-          )
-        </Row>
-      </Container>
-    );
-  }
+    constructor(props) {
+        super();
+        this.props = props;
+    }
+
+    render = () => {
+        return (
+            <Container>
+                <Row>
+                    {this.props.books.map(x => (
+                        <Col xs={3}>
+                            <SingleBook book={x}/>
+                        </Col>)
+                    )
+                    }
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default BookList;
